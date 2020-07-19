59bd152e71f47288b06c081155d99bd057c1e6ef
Brad Geesaman <bradgeesaman+github@gmail.com>
2020-07-19 16:20:26 -0400

    empty roles dir first

--- fetch-all-roles.sh
+++ fetch-all-roles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p roles
+rm -f roles/*
 for i in $(gcloud iam roles list --filter='etag:AA==' --format json | jq -r '.[].name'); do
   echo -n "Describing ${i} ..."
   gcloud iam roles describe "${i}" --format json > "${i}"
